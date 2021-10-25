0ce73722a66df39d2df2b552ea422c4bc681dbeb
cassiasilvaR <cassia.c@ufms.br>
2021-10-24 22:52:17 -0400

    Atualizando

--- Trabalho prático/myapp/app/views.py
+++ Trabalho prático/myapp/app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse #site do danjo?
 # Create your views here.
+def index(request): #O que for colocado aqui vai ser mostrado pro usuário
+    return HttpResponse('<h1> Olá </h1>') #Header
