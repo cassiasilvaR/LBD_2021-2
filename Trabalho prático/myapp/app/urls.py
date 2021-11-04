4b7938d437d14408f1719054a0b4f67253bfd9f7
cassiasilvaR <cassia.c@ufms.br>
2021-11-04 13:29:53 -0400

    Atualizações do index html e counter

--- Trabalho prático/myapp/app/urls.py
+++ Trabalho prático/myapp/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index') #Vai direcionar para a página principal
+    path('', views.index, name='index'), #Vai direcionar para a página principal
+    path('counter', views.counter, name='counter') #Um dos parametros é o nome da função
 ]
--- Trabalho prático/myapp/app/views.py
+++ Trabalho prático/myapp/app/views.py
@@ -1,5 +1,16 @@
+from types import resolve_bases
 from django.shortcuts import render
 from django.http import HttpResponse #site do danjo?
 # Create your views here.
 def index(request): #O que for colocado aqui vai ser mostrado pro usuário
-    return HttpResponse('<h1> Olá </h1>') #Header
+    info = {
+        'nome' : 'Cassia',
+        'idade': 19,
+        'naturalidade' : 'Pará'
+    }
+    return render(request, 'index.html', info) #HttpResponse('<h1> Olá </h1>') #Header
+
+def counter(request):
+    words = request.GET['words']
+    amount_words = len(words.split())
+    return render(request, 'counter.html', {'amount' : amount_words})
