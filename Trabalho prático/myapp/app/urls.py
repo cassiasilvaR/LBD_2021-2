11be02aae309db9960be691a4a060889f1ddbd32
cassiaSilvaR <cassia.c@ufms.br>
2021-11-14 14:19:20 -0400

    Trabalho finalizado

--- Trabalho prático/myapp/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'), #Vai direcionar para a página principal
-    path('counter', views.counter, name='counter') #Um dos parametros é o nome da função
-]
